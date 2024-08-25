2e6c292ecffd7899d1999eef43cfc8e01e85acf4
Salem Adams <salemadamscs@gmail.com>
2024-08-25 17:59:35 -0400

    Added primeng styles

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,21 +1,38 @@
-import {Component} from "@angular/core";
-import {MenubarModule} from "primeng/menubar";
-import {MenuItem} from "primeng/api";
-import {NgIf} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
+import {MenubarModule} from 'primeng/menubar';
+import {BadgeModule} from 'primeng/badge';
+import {AvatarModule} from 'primeng/avatar';
+import {InputTextModule} from 'primeng/inputtext';
+import {CommonModule} from '@angular/common';
+import {RippleModule} from 'primeng/ripple';
 
 @Component({
-  selector: "navbar",
-  templateUrl: "navbar.component.html",
-  imports: [
-    MenubarModule,
-    NgIf
-  ],
-  standalone: true
+    selector: "navbar",
+    templateUrl: "navbar.component.html",
+    standalone: true,
+    imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule]
 })
-export class NavbarComponent {
-  items: MenuItem[] = [
-    { label: 'Home', icon: 'pi-home', link: '/' },
-    { label: 'Project', icon: 'pi-download', link: '/project' },
-    { label: 'Contact', icon: 'pi-user', link: '/contact' },
-  ]
+export class NavbarComponent implements OnInit {
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+                routerLink: '/'
+            },
+            {
+                label: 'Project',
+                icon: 'pi pi-clipboard',
+                routerLink: '/project'
+            },
+            {
+                label: 'Contact',
+                icon: 'pi pi-users',
+                routerLink: 'contact'
+            }
+        ]
+    }
 }
