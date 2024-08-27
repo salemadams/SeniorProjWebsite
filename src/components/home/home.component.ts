b8d4bcfe44a0d26273ab8dd5eb2f7c02961a27f5
Salem Adams <salemadamscs@gmail.com>
2024-08-26 20:18:22 -0400

    Added member cards and contact box

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {CardInformation} from "../../models/CardInformation";
+import {DividerModule} from "primeng/divider";
+import {OverlayPanelModule} from "primeng/overlaypanel";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    ButtonModule,
+    DividerModule,
+    OverlayPanelModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  protected cardInfo: CardInformation[] = [
+    { name: 'Salem Adams', role: 'Developer', description: 'Placeholder', phoneNumber: '470-330-1041', email: 'salemadamscs@gmail.com' },
+    { name: 'Brandon Cantilang', role: 'Developer', description: 'Placeholder', phoneNumber: '', email: '' },
+    { name: 'Nick Rath', role: 'Developer', description: 'Placeholder', phoneNumber: '', email: ''},
+    { name: 'Michael Barber', role: 'Developer', description: 'Placeholder', phoneNumber: '', email: ''},
+  ]
 }
---
+++ src/models/CardInformation.ts
@@ -0,0 +1,7 @@
+export interface CardInformation {
+  name: string;
+  role: string;
+  description: string;
+  phoneNumber: string;
+  email: string;
+}
