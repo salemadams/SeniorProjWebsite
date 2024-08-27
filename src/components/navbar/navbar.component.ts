1a772c52100fc1280c89994cf7f51e67b561a7d8
Salem Adams <salemadamscs@gmail.com>
2024-08-27 18:19:11 -0400

    Added dark and light themes

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -6,17 +6,26 @@ import {AvatarModule} from 'primeng/avatar';
 import {InputTextModule} from 'primeng/inputtext';
 import {CommonModule} from '@angular/common';
 import {RippleModule} from 'primeng/ripple';
+import {InputSwitchModule} from "primeng/inputswitch";
+import {ThemeService} from "../../services/theme.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: "navbar",
     templateUrl: "navbar.component.html",
     standalone: true,
-    imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule]
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, InputSwitchModule, FormsModule]
 })
 export class NavbarComponent implements OnInit {
     items: MenuItem[] | undefined;
+    checked: boolean = true;
+    selectedTheme: string = 'dark';
+
+    constructor(private _themeService: ThemeService) {
+    }
 
     ngOnInit() {
+      this._themeService.switchTheme(this.selectedTheme);
         this.items = [
             {
                 label: 'Home',
@@ -30,4 +39,9 @@ export class NavbarComponent implements OnInit {
             },
         ]
     }
+
+  onThemeChange(theme: string) {
+      this.selectedTheme = theme;
+      this._themeService.switchTheme(theme)
+  }
 }
