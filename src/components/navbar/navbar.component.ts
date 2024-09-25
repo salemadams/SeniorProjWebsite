2cae49d54b467695c9fa5ef861ceb89d73a5e7dd
Salem Adams <salemadamscs@gmail.com>
2024-09-25 19:32:59 -0400

    Changed rules layout and style

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MenubarModule} from 'primeng/menubar';
 import {BadgeModule} from 'primeng/badge';
 import {AvatarModule} from 'primeng/avatar';
@@ -10,12 +10,28 @@ import {FormsModule} from "@angular/forms";
 import {ToolbarModule} from "primeng/toolbar";
 import {Button} from "primeng/button";
 import {InputIconModule} from "primeng/inputicon";
+import {TabMenuModule} from "primeng/tabmenu";
+import {MenuItem} from "primeng/api";
 
 @Component({
     selector: "navbar",
     templateUrl: "navbar.component.html",
     standalone: true,
-  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, InputSwitchModule, FormsModule, ToolbarModule, Button, InputIconModule]
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, InputSwitchModule, FormsModule, ToolbarModule, Button, InputIconModule, TabMenuModule]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] = [
+    { label: 'Home', icon: 'pi pi-home', route: '/' },
+    { label: 'Rules', icon: 'pi pi-map', route: '/rules' },
+    { label: 'Play', icon: 'pi pi-desktop', route: '/game' },
+  ];
+  activeItem: MenuItem | undefined;
+
+  ngOnInit() {
+    this.activeItem = this.items[0];
+  }
+
+  onActiveItemChange(event: MenuItem) {
+    this.activeItem = event;
+  }
 }
